79f86bf2f73da7850421fdfcb1dab55b4084461d
George Appeah <appeahgeorge@gmail.com>
2024-10-22 00:51:35 +0100

    Commit to main - news_tables remine undefined

--- app.py
+++ app.py
@@ -7,16 +7,19 @@ finviz_url_base = 'https://finviz.com/quote.ashx?t='
 
 tickers = ['AMD', 'META', 'AAPL', 'AMZN', 'GOOG', 'MSFT', 'NVDA', 'TSLA', 'SPY']
 
+news_table = {}
+
 for ticker in tickers:
     url = finviz_url_base + ticker
     request = requests.get(url=url, headers={'user-agent': 'my-app/0.0.1'})  # Use requests.get with headers
     print(f'Getting data for {ticker}...')
 
-    # Since you're using requests, urlopen is not necessary. 
     # The request object holds the response, so you can parse it with BeautifulSoup.
     soup = BeautifulSoup(request.text, 'html.parser')
-    print(soup.prettify())  # Visualize the response (HTML) from Finviz
-
+    news_table = soup.find(id='news-table')
+    news_tables[ticker] = news_table
     break
+print(news_tables)  # Visualize the response (HTML) from Finviz
+
     
     
